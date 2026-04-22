ca49d9d9c14f7a4805746479665c45dbb55c685b
chenqingyun <846291219@qq.com>
2026-04-22 14:01:29 +0800

    添加多个函数以演示不同方式创建张量

--- day01/01_张量的基本创建方式.py
+++ day01/01_张量的基本创建方式.py
@@ -1,6 +1,7 @@
 import torch
 import numpy as np
 
+# 1. 定义函数, 演示: torch.tensor 根据指定数据创建张量
 def dm01():
   # 场景1: 标量 张量
   t1 = torch.tensor(10)
@@ -19,5 +20,55 @@ def dm01():
   print(f't3:{t3},type:{type(t3)}')
   print('-' * 30)
 
+# 2. 定义函数, 演示: torch.Tensor 根据形状创建张量, 其也可用来创建指定数据的张量
+def dm02():
+  # 场景1: 标量 张量
+  t1 = torch.Tensor(10)
+  print(f't1:{t1},type:{type(t1)}')
+  print('-' * 30)
+
+  #场景2: 二维列表 ->张量
+  data = [[1,2,3],[4,5,6]]
+  t2 = torch.Tensor(data)
+  print(f't2:{t2},type:{type(t2)}')
+  print('-' * 30)
+
+  #场景3: numpy nd数组——>张量
+  data = np.random.randint(0,10,size=(2,3))
+  t3 = torch.Tensor(data)
+  print(f't3:{t3},type:{type(t3)}')
+  print('-' * 30)
+
+  # 场景4: 尝试直接创建 指定维度(例如: 2行3列的)张量
+  t4 = torch.Tensor(2,3)
+  print(f't4: {t4}, type: {type(t4)}')
+
+# 3. 定义函数, 演示: torch.IntTensor、torch.FloatTensor、torch.DoubleTensor 创建指定类型的张量
+def dm03():
+  # 场景1: 标量 张量
+  t1 = torch.IntTensor(10)
+  print(f't1:{t1},type:{type(t1)}')
+  print('-' * 30)
+
+  #场景2: 二维列表 ->张量
+  data = [[1,2,3],[4,5,6]]
+  t2 = torch.IntTensor(data)
+  print(f't2:{t2},type:{type(t2)}')
+  print('-' * 30)
+
+  #场景3: numpy nd数组——>张量
+  data = np.random.randint(0,10,size=(2,3))
+  t3 = torch.IntTensor(data)
+  print(f't3:{t3},type:{type(t3)}')
+  print('-' * 30)
+
+  # 场景4: 如果类型不匹配, 会尝试自动转换类型.
+  data = np.random.randint(0,10,size=(2,3))
+  t4 = torch.FloatTensor(data) # 默认float32
+  print(f't4:{t4},type:{type(t4)}')
+  print('-' * 30)
+
 if __name__ == '__main__':
-  dm01()
+  # dm01() # 实际使用场景更多，优先掌握
+  # dm02()
+  dm03()
